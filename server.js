64daa58ea72cce097307f1a313cf611af1a63e9d
nicoHikaru <nicolas.quei83@gmail.com>
2024-07-09 19:35:37 +0200

    update

--- server.js
+++ server.js
@@ -2,40 +2,39 @@ import puppeteer from 'puppeteer';
 
 (async () => {
     // Launch the browser and open a new blank page
-    const browser = await puppeteer.launch({headless: true});
+    const browser = await puppeteer.launch({headless: false});
     const page = await browser.newPage();
 
     // Navigate the page to a URL
-    await page.goto('https://www.google.com/');
+    await page.goto('https://www.google.com/',{waitUntil: "networkidle0", timeout: 90000});
 
-    // Query for an element handle.
-    const element = await page.waitForSelector('#L2AGLb');
-
-    // Do something with element...
-    await element.click(); // Just an example.
+    const element = await page.waitForSelector('#L2AGLb');//accepter conditio utilisateur
+    
+    await element.click(); 
 
     await page.evaluate(() => {
-        document.querySelector(".gLFyf").value = "restaurant japonais à paris";
+        document.querySelector(".gLFyf").value = "plomberie toulon";//champs recherche
     });
 
-    await page.waitForSelector('.gLFyf'); // <-- wait until it exists
-    await page.click(".gLFyf");  
+    await page.waitForSelector('.gLFyf');
+    await page.click(".gLFyf");
 
-    const searchResultSelector = '.gNO89b';
-    console.log('buton valide recherche',searchResultSelector);
-    const button = await page.waitForSelector(searchResultSelector);
-    console.log('buton valide recherche',button);
+    
+    const searchResultSelector = '.gNO89b';// class css button lancer la recherche
+    
+    await page.waitForSelector(searchResultSelector);
     await page.click(searchResultSelector);
 
-    await page.waitForSelector('.gLFyf'); // <-- wait until it exists
+    await page.waitForSelector('.gLFyf');
 
-    await page.waitForSelector('.sVXRqc'); // <-- wait until it exists
+    await page.waitForSelector('.sVXRqc');
     const hrefs = await page.$$eval(
         ".sVXRqc",
         els => els.map(el => el.href)
       );
+    
     console.log(hrefs);
     console.log("fini");
 
-    await browser.close();
+    //await browser.close();
 })();
